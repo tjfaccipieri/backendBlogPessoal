5145188632e4960311df1f180389a1b8c24297c1
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2021-12-10 11:58:24 -0300

    getById User

--- src/main/java/br/org/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/br/org/generation/blogpessoal/controller/UsuarioController.java
@@ -41,7 +41,7 @@ public class UsuarioController {
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Usuario> getById(@PathVariable long id) {
-		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+		return usuarioRepository.findById(id).map(resp -> ResponseEntity.ok(resp))
 		.orElse(ResponseEntity.notFound().build());
 	}
 	
