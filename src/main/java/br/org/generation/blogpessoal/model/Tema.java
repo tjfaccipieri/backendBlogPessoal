417e5ecbf71fed4535bb78f55f3acf507cfe78ba
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2022-02-04 07:56:59 -0300

    deploy

--- src/main/java/br/org/generation/blogpessoal/model/Tema.java
+++ src/main/java/br/org/generation/blogpessoal/model/Tema.java
@@ -24,7 +24,7 @@ public class Tema{
 		@NotNull(message = "O atributo Descrição deve ser obrigatório")
 		private String descricao;
 		
-		@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+		@OneToMany(mappedBy = "tema", cascade = CascadeType.MERGE)
 		@JsonIgnoreProperties({"tema"})
 		private List<Postagem> postagem;
 		
